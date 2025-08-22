3937f334ab1a4799d9bc31eacd836f45a511f1ec
Pedro Martinez <peter.mtz.gtz@gmail.com>
2025-08-22 16:35:58 -0600

    Create Introduccion_O2025.py

---
+++ Introduccion_O2025.py
@@ -0,0 +1,8 @@
+# Hola, bienvenidos a un nuevo semestre.
+# este programa ayudara a practicar conceptos clave de
+# Github.
+
+print('Hello world from GitHub /n Usuario: Pedro Mtz')
+
+# Insertar su nombre
+print('Hello world from GitHub /n (usuario)')
